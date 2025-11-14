d42c8d7047d306e436feb004ac72cb2ad16f943a
tatpow <69532548+tatpow@users.noreply.github.com>
2025-11-14 19:37:56 +0300

    Update to v1.3.0 | Audio Change Everything

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from app.ui.window import Window
 from app.data import variables
 
+import os
+import sys
+
+if getattr(sys, 'frozen', False):
+    # Путь к исполняемому файлу
+    app_path = os.path.dirname(sys.executable)
+    internal_path = os.path.join(app_path, '_internal')
+    
+    # Добавляем в PATH
+    os.environ['PATH'] = internal_path + os.pathsep + os.environ['PATH']
+
 def run():
     application = Window() # Создание приложения из окна
 
